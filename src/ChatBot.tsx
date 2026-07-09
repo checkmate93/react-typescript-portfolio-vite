ef925663fd109e2a0b6fcf220e018fb689db1e57
E.MANTOUSIS <152896552+checkmate93@users.noreply.github.com>
2026-07-10 02:57:15 +0300

    Change body key from 'message' to 'messages'

--- src/ChatBot.tsx
+++ src/ChatBot.tsx
@@ -22,7 +22,7 @@ export default function ChatBot() {
         headers: { 
           "Content-Type": "application/json" 
         },
-        body: JSON.stringify({ message: currentMsg }),
+        body: JSON.stringify({ messages: currentMsg }),
       });
 
       if (!res.ok) {
